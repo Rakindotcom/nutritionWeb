ce85fc78bd903999aa2da81c280207760b3f068c
Rakindotcom <rakinalshahriar2004@gmail.com>
2025-03-26 15:49:55 +0600

    initialize React project with Vite, Tailwind CSS, and basic components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+    <h1 className='text-4xl'>Hi lorem45</h1>
+    </>
+  )
+}
+
+export default App
